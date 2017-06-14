07559acf6b2db795f261a46002dc900ed345bf6c
Spencer Richards <spencer.richards7@gmail.com>
2017-06-14 10:12:51 -0600

    axios working

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,6 +1,15 @@
 import React from 'react';
+var axios = require('axios')
 
 class Product extends React.Component {
+
+  componentDidMount() {
+    axios.get('http://challenge.eliteworks.com/product?api_key=spencerrichards34_5xs@indeedemail.com')
+      .then(function(response) {
+        console.log(response)
+      })
+  }
+
   render() {
     return(
       <div>
