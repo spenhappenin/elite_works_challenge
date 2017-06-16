cad67bf00c39c39f36747b5f5f6e54558799de43
Spencer Richards <spencer.richards7@gmail.com>
2017-06-16 13:39:21 -0600

    App Show and Edit working

--- src/App.js
+++ src/App.js
@@ -1,30 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Product from './components/Product';
 import $ from "jquery";
-import axios from 'axios';
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { product: {} }
+    this.state = { product: {}, edit: false }
+
+    this.toggleEdit = this.toggleEdit.bind(this);
+    this.handleEdit = this.handleEdit.bind(this);
   }
 
   componentDidMount() {
-
-    // axios.get("http://challenge.eliteworks.com/product?api_key=spencerrichards34_5xs@indeedemail.com")
-    //   .then(resp => {
-    //     let product = resp.data.data.product;
-    //     let string = product.data.replace(/'/g, '"');
-    //     product.data = JSON.parse(string);
-    //     debugger;
-    //     this.setState({ product })        
-    //   })
-    //   .catch(err => {
-    //     console.log(err)
-    //   })
-
     $.ajax({
       url: "http://challenge.eliteworks.com/product?api_key=spencerrichards34_5xs@indeedemail.com", 
       type: "GET",
@@ -39,14 +27,72 @@ class App extends Component {
     });
   }
 
-  render() {
-    return (
+  toggleEdit() {
+    this.setState({ edit: !this.state.edit });
+  }
+
+  handleEdit() {
+    let name = this.refs.editName.value;
+    let description = this.refs.editDescription.value;
+    let imageUrl = this.refs.editImageUrl.value;
+    let food = this.refs.editFood.value;
+    let animal = this.refs.editAnimal.value;
+    let data = `{'imageUrl': '${imageUrl}', 'food': '${food}', 'animal': '${animal}'}`;
+    
+    $.ajax({
+      url: "http://challenge.eliteworks.com/product/set?api_key=spencerrichards34_5xs@indeedemail.com",
+      type: "POST",
+      dataType: "JSON",
+      data: { name, description, data: data }
+    }).done( data => {
+      // debugger;
+      console.log("Success!");
+      this.setState({ product: {name: name, description: description, data: {imageUrl: imageUrl, food: food, animal: animal} }})
+      this.toggleEdit();
+    }).fail( err => {
+      console.log(err);
+    });
+  }
+
+  display() {
+    return(
+      <div className="App">
+        <div className="App-intro">
+          <Product product={this.state.product} />
+          <button onClick={ this.toggleEdit }>Edit</button>
+        </div>
+      </div>
+    );
+  }
+
+  edit() {
+    return(
       <div className="App">
         <p className="App-intro">
-          <Product product={this.state.product} />
+          <input type='text' ref='editName' defaultValue={ this.state.product.name } placeholder='Name' />
+          <br />
+          <input type='text' ref='editDescription' defaultValue={ this.state.product.description } placeholder='Description' />
+          <br />
+          <input type='text' ref='editImageUrl' defaultValue={ this.state.product.data.imageUrl } placeholder='Image URL' />
+          <br />
+          <input type='text' ref='editFood' defaultValue={ this.state.product.data.food } placeholder='Food' />
+          <br />          
+          <input type='text' ref='editAnimal' defaultValue={ this.state.product.data.animal } placeholder='Animal' />
+          <br />          
+          <button onClick={ this.toggleEdit }>Cancel</button>
+          <br />
+          <button onClick={ this.handleEdit }>Edit</button>
         </p>
       </div>
     );
+  }
+
+  render() {
+    if(this.state.edit) {
+      return( this.edit() );
+    } else {
+      return( this.display() );
+    }
   }
 }
 
--- src/components/Product.js
+++ src/components/Product.js
@@ -1,23 +1,40 @@
 import React from 'react';
 
-const Product = ({ product }) => {
-  console.log(product.data);
-
+const Product = ({ product }) => {  
   if(!product.data) {
     return(
-      <div>Loading</div>
+      <div>Loading...</div>
     )
   }
-  let { name, description, data: { imageUrl, food, animal} } = product
 
+  let { name, description, data: { imageUrl, food, animal} } = product
   return(
-    <div>
-      <div>
-        <h1>{ name }</h1>
-        <h1>{ description }</h1>
-        <img src={imageUrl} width='300' />
-        <h1>{ food }</h1>
-        <h1>{ animal }</h1>
+    <div className='container'>
+      <div className="card-container">
+        <div>
+          <img src={imageUrl} width='350' alt='Product' />
+        </div>
+
+        <div className='product-title'>
+          <div className='card-content'>
+            <h2>{ name }</h2>
+          </div>
+          <div className='card content'>
+            <p>{ description }</p>
+          </div>
+          <hr />
+        </div>
+
+        <div className='product-favorites'>
+          <div className='card-content'>
+            <h5>Favorite Food: </h5> 
+            <p>{ food }</p>
+          </div>
+          <div className='card-content'>
+            <h5>Favorite Animal: </h5>
+            <p>{ animal }</p>
+          </div>
+        </div>
       </div>
     </div>
   );
